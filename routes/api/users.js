64a29d858d42388ba713a0e1e43e79c73b58f205
Zixu (Shawn) Chen <zixu.chen.shawn@jhu.edu>
2018-11-05 17:36:03 -0500

    Add some register validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// Load input validation
+const validateRegisterInput = require('../../validation/register');
+
 // Load User Model
 const User = require('../../models/User');
 
@@ -18,10 +21,18 @@ router.get('/test', (req, res) => res.json({ msg: 'Users works' }));
 // @description Register the user / hash the password
 // @access      Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     // User already exists
     if (user) {
-      return res.status(400).json({ email: 'Email already exists' });
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
     } else {
       // Create link of avatar
       const avatar = gravatar.url(req.body.email, {
