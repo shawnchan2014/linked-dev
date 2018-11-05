61acc10cad3f8d0bfe77b3c8b13c951bf57b80d5
Zixu (Shawn) Chen <zixu.chen.shawn@jhu.edu>
2018-11-05 14:49:09 -0500

    Finish creating JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://shawn:123shawn@ds263710.mlab.com:63710/devconnector'
+  mongoURI: 'mongodb://shawn:123shawn@ds263710.mlab.com:63710/devconnector',
+  secretOrKey: 'secret'
 };
