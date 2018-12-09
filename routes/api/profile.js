d3eb68137db6d177888c2f4a9aa20c6ba5510ee2
Zixu (Shawn) Chen <zixu.chen.shawn@jhu.edu>
2018-12-09 00:21:32 -0500

    Add routes for adding experience and education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,6 +9,8 @@ const User = require('../../models/User');
 
 // Load validation
 const validateProfileInput = require('../../validation/profile');
+const validateExperienceInput = require('../../validation/experience');
+const validateEducationInput = require('../../validation/education');
 
 // @route       GET /api/profile/test
 // @description Tests profile route
@@ -159,6 +161,87 @@ router.post(
       }
     });
   }
+);
+
+// @route       POST /api/profile/experience
+// @description Add experience to profile
+// @access      Private
+router.post(
+  '/experience',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      // Return any errors
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      // Add to experience array
+      profile.experience.unshift(newExp);
+
+      profile.save().then(profile => {
+        res.json(profile);
+      });
+    });
+  }
+);
+
+// @route       POST /api/profile/education
+// @description Add education to profile
+// @access      Private
+router.post(
+  '/education',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      // Return any errors
+      return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newEdu = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldofstudy: req.body.fieldofstudy,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      // Add to education array
+      profile.education.unshift(newEdu);
+
+      profile.save().then(profile => {
+        res.json(profile);
+      });
+    });
+  }
+);
+
+// @route       DELETE /api/profile/experience/:exp_id
+// @description Delete experience from profile
+// @access      Private
+router.post(
+  '/education/:exp_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {}
 );
 
 module.exports = router;
