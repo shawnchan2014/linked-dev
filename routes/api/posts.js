e656760c8bbdaad6976c9b59b5c9d79bb7e264e0
Zixu (Shawn) Chen <zixu.chen.shawn@jhu.edu>
2018-12-09 15:27:09 -0500

    Add create post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Post model
+const Post = require('../../models/Post');
+
+// Validation
+const validatePostInput = require('../../validation/post');
 
 // @route       GET /api/posts/test
 // @description Tests posts route
 // @access      Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts works' }));
+
+// @route       POST /api/posts
+// @description Create post
+// @access      Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      user: req.user.id,
+      text: req.body.text,
+      name: req.body.name, // Redux will get user's name and avatar
+      avatar: req.body.avatar
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
