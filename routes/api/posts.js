d3f3056ec79a1eb4f47601b688d9569e08460446
Zixu (Shawn) Chen <zixu.chen.shawn@jhu.edu>
2018-12-09 17:28:04 -0500

    Add add and delete comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -161,4 +161,87 @@ router.post(
   }
 );
 
+// @route       POST /api/posts/comment/:id
+// @description Comment post
+// @access      Private
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          user: req.user.id,
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar
+        };
+
+        // Add to comment array
+        post.comments.unshift(newComment);
+
+        // Save
+        post
+          .save()
+          .then(post => res.json(post))
+          .catch(err =>
+            res.status(500).json({ error: 'Error saving comment' })
+          );
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+  }
+);
+
+// @route       DELETE /api/posts/comment/:id/:comment_id
+// @description Delete comment from post
+// @access      Private
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        // Check to see if comment exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res.status(404).json({
+            commentnotexists: 'Comment does not exist'
+          });
+        }
+
+        // Get remove index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        if (post.comments[removeIndex].user.toString() !== req.user.id) {
+          return res
+            .status(400)
+            .json({ unauthorized: "Cannot remove others' comment" });
+        }
+
+        // Splice out of array
+        post.comments.splice(removeIndex, 1);
+
+        post
+          .save()
+          .then(post => res.json(post))
+          .catch(err =>
+            res.status(500).json({ error: 'Error deleting comment' })
+          );
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+  }
+);
+
 module.exports = router;
