cf630ba0686b92423fb8169109f0cdea2f7208ab
Zixu (Shawn) Chen <zixu.chen.shawn@jhu.edu>
2018-12-23 00:51:15 -0500

    Add loginUser in authAction

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
-import { GET_ERRORS } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Register user
 export const registerUser = (userData, history) => dispatch => {
@@ -14,3 +16,39 @@ export const registerUser = (userData, history) => dispatch => {
       })
     );
 };
+
+// Login - Get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      // Save to local storage
+      const { token } = res.data;
+      // Set token to local storage
+      localStorage.setItem('jwtToken', token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch({
+        type: SET_CURRENT_USER,
+        payload: decoded
+      });
+      //dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set logged in user
+// export const setCurrentUser = decoded => {
+//   return {
+//     type: SET_CURRENT_USER,
+//     payload: decoded
+//   };
+// };
