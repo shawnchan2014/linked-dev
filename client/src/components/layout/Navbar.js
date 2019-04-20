cf41de1625ee20e6a86065a3795941664a60627d
Zixu (Shawn) Chen <zixu.chen.shawn@jhu.edu>
2019-04-19 20:41:09 -0400

    Set up profile reducer

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logoutUser } from '../../actions/authActions';
+import { clearCurrentProfile } from '../../actions/profileActions';
 
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
 
@@ -14,18 +16,18 @@ class Navbar extends Component {
     const { isAuthenticated, user } = this.props.auth;
 
     const authLinks = (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
+      <ul className='navbar-nav ml-auto'>
+        <li className='nav-item'>
           <a
-            href="/logout"
+            href='/logout'
             onClick={this.onLogoutClick.bind(this)}
-            className="nav-link"
+            className='nav-link'
           >
             <img
-              className="rounded-circle"
+              className='rounded-circle'
               src={user.avatar}
               alt={user.name}
-              title="You must have a Gravatar connected to your email to display an image"
+              title='You must have a Gravatar connected to your email to display an image'
               style={{ width: '25px', marginRight: '5px' }}
             />
             Logout
@@ -35,14 +37,14 @@ class Navbar extends Component {
     );
 
     const guestLinks = (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/register">
+      <ul className='navbar-nav ml-auto'>
+        <li className='nav-item'>
+          <Link className='nav-link' to='/register'>
             Sign Up
           </Link>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">
+        <li className='nav-item'>
+          <Link className='nav-link' to='/login'>
             Login
           </Link>
         </li>
@@ -50,24 +52,24 @@ class Navbar extends Component {
     );
 
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
+      <nav className='navbar navbar-expand-sm navbar-dark bg-dark mb-4'>
+        <div className='container'>
+          <Link className='navbar-brand' to='/'>
             LinkedDev
           </Link>
           <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#mobile-nav"
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#mobile-nav'
           >
-            <span className="navbar-toggler-icon" />
+            <span className='navbar-toggler-icon' />
           </button>
 
-          <div className="collapse navbar-collapse" id="mobile-nav">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/profiles">
+          <div className='collapse navbar-collapse' id='mobile-nav'>
+            <ul className='navbar-nav mr-auto'>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/profiles'>
                   {' '}
                   Developers
                 </Link>
@@ -91,7 +93,8 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {
-  logoutUser
+  logoutUser,
+  clearCurrentProfile
 };
 
 export default connect(
