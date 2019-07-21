6682bd3a7f76e9f8222576c4ffd31997e10fae6c
Zixu (Shawn) Chen <zixu.chen.shawn@jhu.edu>
2019-07-20 22:29:40 -0400

    Profile basics

---
+++ client/src/components/profile/ProfileGitHub.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ProfileGitHub extends Component {
+  static propTypes = {
+    prop: PropTypes
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Profile GitHub</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileGitHub;
