f23f9730b7a7de97b966dcbc113609365b650852
Zixu (Shawn) Chen <zixu.chen.shawn@jhu.edu>
2019-07-22 00:21:28 -0400

    Fix github display bugs

--- client/src/components/profile/ProfileGitHub.js
+++ client/src/components/profile/ProfileGitHub.js
@@ -22,7 +22,7 @@ class ProfileGitHub extends Component {
     )
       .then(res => res.json())
       .then(data => {
-        if (this.refs.myRefs) {
+        if (this.refs.myRef) {
           this.setState({ repos: data });
         }
       })
@@ -53,10 +53,10 @@ class ProfileGitHub extends Component {
               Stars: {repo.stargazers_count}
             </span>
             <span className='badge badge-secondary mr-1'>
-              Stars: {repo.watchers_count}
+              Watchers: {repo.watchers_count}
             </span>
             <span className='badge badge-success'>
-              Stars: {repo.forks_count}
+              Forks: {repo.forks_count}
             </span>
           </div>
         </div>
@@ -64,7 +64,7 @@ class ProfileGitHub extends Component {
     ));
 
     return (
-      <div refs='myRefs'>
+      <div ref='myRef'>
         <hr />
         <h3 className='mb-4'>Latest GitHub Repos</h3>
         {repoItems}
